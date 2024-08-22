dffe5b192cae73c094f2c1d962158858b7fa641d
Estalin Ulco <estalin_612@hotmail.com>
2024-08-22 00:37:15 -0500

    cambio de carpeta de angularmaterial modulo

--- src/app/v-presentacion/presentacion.module.ts
+++ src/app/v-presentacion/presentacion.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { Ventana1Component } from './components/ventana1/ventana1.component';
-import { AngularMaterialModule } from './angularmaterial.module';
+import { AngularMaterialModule } from '../angularmaterial.module';
 
 
 
